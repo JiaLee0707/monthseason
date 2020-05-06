496e46c11241ace823e8dbe80e425748cc9119bf
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-05-06 21:57:42 +0900

    NCP 2020.05.06.(수) 실습일지_6번

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,6 +10,35 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
+            Console.Write("몇 월? ");
+            var month = Console.ReadLine();
+
+            int num = 0;
+            if (!(month.Contains("월") || month.Contains("0") || int.TryParse(month, out num)))
+            {
+                Console.WriteLine("잘못입력하셨습니다...");
+            }
+            else
+            {
+                if (month.Contains("3") || month.Contains("4") || month.Contains("5"))
+                {
+                    Console.WriteLine("봄");
+                }
+                else if (month.Contains("6") || month.Contains("7") || month.Contains("8"))
+                {
+                    Console.WriteLine("여름");
+                }
+                else if (month.Contains("9") || month.Contains("10") || month.Contains("11"))
+                {
+                    Console.WriteLine("가을");
+                }
+                else if (month.Contains("12") || month.Contains("1") || month.Contains("2"))
+                {
+                    Console.WriteLine("겨울");
+                }
+            }
+
+
         }
     }
 }
